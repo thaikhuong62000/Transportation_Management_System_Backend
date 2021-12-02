58c4d0e439714bbcfe455dcda72f0388d42505bd
thaikhuong62000 <thaikhuong62000@gmail.com>
2021-12-02 11:01:44 +0700

    Init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0441af3dad953748a97088752b630471'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '626d0d93-aa65-4e35-af66-7ba3f90ff10c'
+};
