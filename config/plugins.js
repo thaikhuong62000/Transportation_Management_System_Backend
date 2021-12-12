2637a447d21172476455a9b958e1f2c1b6d63d32
thaikhuong62000 <thaikhuong62000@gmail.com>
2021-12-12 20:46:46 +0700

    .

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,14 @@
 module.exports = ({ env }) => ({
-  upload: {
-    provider: "cloudinary",
-    providerOptions: {
-      cloud_name: env("CLOUDINARY_NAME", "dfnoohdaw"),
-      api_key: env("CLOUDINARY_KEY", 112164677597438),
-      api_secret: env("CLOUDINARY_SECRET", "iKHO6w77OmHhAuT4UEsEqigHkYM"),
-    },
-    actionOptions: {
-      upload: {},
-      delete: {},
-    },
-  },
+  // upload: {
+  //   provider: "cloudinary",
+  //   providerOptions: {
+  //     cloud_name: env("CLOUDINARY_NAME", "dfnoohdaw"),
+  //     api_key: env("CLOUDINARY_KEY", 112164677597438),
+  //     api_secret: env("CLOUDINARY_SECRET", "iKHO6w77OmHhAuT4UEsEqigHkYM"),
+  //   },
+  //   actionOptions: {
+  //     upload: {},
+  //     delete: {},
+  //   },
+  // },
 });
--- config/server.js
+++ config/server.js
@@ -1,9 +1,15 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '0441af3dad953748a97088752b630471'),
+      secret: env("ADMIN_JWT_SECRET", "0441af3dad953748a97088752b630471"),
+    },
+  },
+  settings: {
+    cors: {
+      enabled: true,
+      origin: ["*"],
     },
   },
 });
