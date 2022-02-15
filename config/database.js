fec9af438f20c5abdc4ce02805e9c8fa4ed765c2
thaikhuong62000 <thaikhuong62000@gmail.com>
2022-02-15 09:03:12 +0700

    Add local database config

--- config/database.js
+++ config/database.js
@@ -1,19 +1,34 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: "mongoose",
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.t5q90.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'tms'),
-        username: env('DATABASE_USERNAME', 'thaikhuong'),
-        password: env('DATABASE_PASSWORD', 'Uj8i5uvFTY47rDS'),
+        host: env("DATABASE_HOST", "cluster0.t5q90.mongodb.net"),
+        srv: env.bool("DATABASE_SRV", true),
+        port: env.int("DATABASE_PORT", 27017),
+        database: env("DATABASE_NAME", "tms"),
+        username: env("DATABASE_USERNAME", "thaikhuong"),
+        password: env("DATABASE_PASSWORD", "Uj8i5uvFTY47rDS"),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+        ssl: env.bool("DATABASE_SSL", true),
+      },
+    },
+    local_db: {
+      connector: "mongoose",
+      settings: {
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        srv: env.bool("DATABASE_SRV", false),
+        port: env.int("DATABASE_PORT", 27017),
+        database: env("DATABASE_NAME", "tms_local"),
+        username: env("DATABASE_USERNAME", null),
+        password: env("DATABASE_PASSWORD", null),
+      },
+      options: {
+        authenticationDatabase: env("AUTHENTICATION_DATABASE", "admin"),
+        ssl: env.bool("DATABASE_SSL", false),
       },
     },
   },
