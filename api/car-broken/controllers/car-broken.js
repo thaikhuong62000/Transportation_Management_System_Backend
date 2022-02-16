0faee5cfb08bb20d032761b4df13405a5f472fc5
thaikhuong62000 <thaikhuong62000@gmail.com>
2022-02-16 16:02:56 +0700

    Modify create method for car_broken

--- api/car-broken/controllers/car-broken.js
+++ api/car-broken/controllers/car-broken.js
@@ -1,8 +1,35 @@
-'use strict';
+"use strict";
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Create a record in table car_broken.
+   *
+   * @return {Object}
+   */
+  async create(ctx) {
+    // Validation
+    const data = ctx.request.body;
+    if (new Date(data.time) === null) {
+      // 400 Bad Request
+      ctx.response.status = 400;
+      // Wrong time format
+      // ctx.response.message = "Wrong time format";
+      return;
+    }
 
-module.exports = {};
+    try {
+      await strapi.services.car.findOne({ id: data.car });
+    } catch (error) {
+      // 400 Bad Request
+      ctx.response.status = 400;
+      // Car not found
+      // ctx.response.message = "Car not found";
+      return;
+    }
+
+    // Add Car_broken
+    await strapi.services["car-broken"].create(data);
+    ctx.response.status = 201;
+    return;
+  },
+};
