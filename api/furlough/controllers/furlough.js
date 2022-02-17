655062e7dc865e6b65ffdfdb1eeccee012df09c0
AnhKhoa1408 <akhoa981@gmail.com>
2022-02-17 20:48:08 +0700

    Add create furlough

--- api/furlough/controllers/furlough.js
+++ api/furlough/controllers/furlough.js
@@ -1,8 +1,44 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async create(ctx) {
+    const {
+      id,
+      start_time,
+      end_time,
+      reason = "",
+      state = "pending",
+    } = ctx.request.body;
+
+    if (new Date(start_time) > new Date(end_time)) {
+      return ctx.badRequest("Start time must not be larger than end time!");
+    }
+
+    if (new Date(start_time) < new Date()) {
+      return ctx.badRequest("Invalid start time!");
+    }
+
+    const user = await strapi
+      .query("user", "users-permissions")
+      .findOne({ _id: id });
+
+    if (!user) {
+      return ctx.badRequest(null, "Driver not found");
+    }
+
+    const furlough = await strapi.query("furlough").create({
+      state,
+      start_time,
+      end_time,
+      reason,
+      driver: user,
+    });
+
+    return furlough;
+  },
+};
