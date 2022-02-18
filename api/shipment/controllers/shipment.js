4fc7c1d47d557dd0fbd9f7e412adb86a45babe6e
AnhKhoa1408 <akhoa981@gmail.com>
2022-02-18 20:29:55 +0700

    Add api get vehicle detail by shipment for stocker

--- api/shipment/controllers/shipment.js
+++ api/shipment/controllers/shipment.js
@@ -55,4 +55,24 @@ module.exports = {
       })
     );
   },
+
+  async getVehicleDetailByShipment(ctx) {
+    const { id } = ctx.params;
+
+    let shipmentDetail = await strapi.query("shipment").findOne({ id: id });
+
+    shipmentDetail = sanitizeEntity(shipmentDetail, {
+      model: strapi.models.shipment,
+      includeFields: ["packages", "to_address", "driver", "assistance"],
+    });
+
+    const totalWeight = shipmentDetail.packages.reduce((total, item) => {
+      return total + item.weight;
+    }, 0);
+
+    return {
+      total_weight: totalWeight,
+      ...shipmentDetail,
+    };
+  },
 };
