67191aab246f0966bd6cca3d121c5ba27790f5cf
thaikhuong62000 <thaikhuong62000@gmail.com>
2022-02-22 18:10:31 +0700

    Init chat

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * An asynchronous bootstrap function that runs before
@@ -10,4 +10,25 @@
  * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+module.exports = () => {
+  var io = require("socket.io")(
+    strapi.server
+    // , {
+    //   cors: {
+    //     origin: "http://localhost:3000",
+    //     methods: ["GET", "POST"],
+    //     allowedHeaders: ["my-custom-header"],
+    //     credentials: true,
+    //   },
+    // }
+  );
+  // https://strapi.io/blog/how-to-build-a-real-time-chat-forum-using-strapi-socket-io-react-and-mongo-db
+  // https://dev.to/kris/buiding-chat-app-with-react-native-and-socket-io-4p8l
+  io.on("connection", function (socket) {
+    socket.on("chat", ({ username, room }) => {
+      console.log("user connected");
+      console.log("username is ", username);
+      console.log("room is...", room);
+    });
+  });
+};
