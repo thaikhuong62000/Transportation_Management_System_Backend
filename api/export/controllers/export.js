e3063dbc8bc78b0154760711b563ff18478df6c4
AnhKhoa1408 <akhoa981@gmail.com>
2022-03-05 17:09:29 +0700

    add update quantity for export

--- api/export/controllers/export.js
+++ api/export/controllers/export.js
@@ -23,4 +23,45 @@ module.exports = {
       })
     );
   },
+
+  async updateExportQuantityByPackage(ctx) {
+    const { packageId, quantity } = ctx.request.body;
+    const { storage, id } = ctx.state.user;
+
+    if (!quantity && quantity < 0) {
+      return ctx.badRequest([
+        {
+          id: "import.updateImportQuantityByPackage",
+          message: "Invalid package quantity",
+        },
+      ]);
+    }
+
+    let exportedPackage = await strapi
+      .query("export")
+      .model.findOneAndUpdate(
+        { package: packageId },
+        { quantity: quantity },
+        { new: true }
+      );
+
+    if (!exportedPackage) {
+      let newExport = await strapi.query("export").create({
+        quantity: quantity,
+        store_manager: id,
+        package: packageId,
+        storage: storage,
+      });
+
+      return sanitizeEntity(newExport, {
+        model: strapi.query("export").model,
+        includeFields: ["quantity"],
+      });
+    }
+
+    return sanitizeEntity(exportedPackage, {
+      model: strapi.query("export").model,
+      includeFields: ["quantity"],
+    });
+  },
 };
