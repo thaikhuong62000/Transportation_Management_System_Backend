8ddff9c9f1bf75504dfa5054d75e495509c815ea
AnhKhoa1408 <akhoa981@gmail.com>
2022-03-10 23:24:38 +0700

    Initialize for payment by momo wallet

--- api/payment/controllers/payment.js
+++ api/payment/controllers/payment.js
@@ -1,8 +1,22 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async momoNotification(ctx) {
+    const { resultCode, message } = ctx.request.body;
+    // TODO: Response to momo ipn post and check for payment status
+    // TODO: Validate amount and currency in database and momo body
+
+    return ctx.send(
+      {
+        resultCode: resultCode,
+        message: message,
+      },
+      204
+    );
+  },
+};
