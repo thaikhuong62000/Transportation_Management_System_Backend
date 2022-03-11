b3e4bb7fdb4ffdbe3f31154d8e43d87933bf7ee6
AnhKhoa1408 <akhoa981@gmail.com>
2022-03-11 21:46:49 +0700

    update payment for momo

--- api/payment/controllers/payment.js
+++ api/payment/controllers/payment.js
@@ -1,4 +1,5 @@
 "use strict";
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
@@ -13,9 +14,31 @@ module.exports = {
       resultCode = 0,
     } = ctx.request.body;
 
-    console.log(ctx.request.body);
-    console.log(Buffer.from(ctx.request.body.id, "base64").toString("ascii"));
+    let rawData = Buffer.from(extraData, "base64").toString("ascii");
+    let parsedId = JSON.parse(rawData).id;
 
+    let order = await strapi.query("order").findOne({
+      id: parsedId,
+    });
+
+    if (resultCode === 0) {
+      if (order.fee >= amount) {
+        await strapi
+          .query("order")
+          .update({ id: parsedId }, { remain_fee: order.fee - amount });
+
+        await strapi.query("payment").update(
+          { order: parsedId },
+          {
+            paid: amount,
+            time: new Date(),
+          }
+        );
+      }
+    } else {
+      await strapi.query("order").delete({ id: parsedId });
+      await strapi.query("payment").delete({ _id: order.payments[0]._id });
+    }
     return ctx.send(204);
   },
 };
