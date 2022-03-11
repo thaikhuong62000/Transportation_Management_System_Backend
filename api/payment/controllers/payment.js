e879bd6ec2cc8706d03cf466a5ca7baab53afc77
AnhKhoa1408 <akhoa981@gmail.com>
2022-03-11 20:52:31 +0700

    add payment momo

--- api/payment/controllers/payment.js
+++ api/payment/controllers/payment.js
@@ -1,5 +1,4 @@
 "use strict";
-
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
@@ -7,18 +6,16 @@
 
 module.exports = {
   async momoNotification(ctx) {
-    const { resultCode, message } = ctx.request.body;
-    // TODO: Response to momo ipn post and check for payment status
-    // TODO: Validate amount and currency in database and momo body
+    const {
+      amount = 0,
+      extraData = "",
+      message = "",
+      resultCode = 0,
+    } = ctx.request.body;
 
     console.log(ctx.request.body);
+    console.log(Buffer.from(ctx.request.body.id, "base64").toString("ascii"));
 
-    return ctx.send(
-      {
-        resultCode: resultCode,
-        message: message,
-      },
-      204
-    );
+    return ctx.send(204);
   },
 };
