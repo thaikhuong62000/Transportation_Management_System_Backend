0cc1ab281a747299e4fdf94f6e8ccb3ccb314f48
AnhKhoa1408 <akhoa981@gmail.com>
2022-03-13 23:24:17 +0700

    add order template api

--- api/order-template/services/order-template.js
+++ api/order-template/services/order-template.js
@@ -1,8 +1,20 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  async getOrderTemplatesByUser(id, skip, limit) {
+    let entities = await strapi
+      .query("order-template")
+      .model.find({
+        user: id,
+      })
+      .skip(skip)
+      .limit(limit);
+
+    return entities;
+  },
+};
