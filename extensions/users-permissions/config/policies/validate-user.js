59f5c4e57f69f8cac74ed409dee2e07cd66e6adc
AnhKhoa1408 <akhoa981@gmail.com>
2022-03-18 22:24:43 +0700

    remove console log

--- extensions/users-permissions/config/policies/validate-user.js
+++ extensions/users-permissions/config/policies/validate-user.js
@@ -4,8 +4,6 @@ module.exports = async (ctx, next) => {
   let { role, id } = ctx.state.user;
   let updateId = ctx.params.id;
 
-  console.log(id, updateId);
-
   if (["Customer", "Driver", "Stocker", "Assistance"].includes(role.name)) {
     if (updateId !== id) {
       return ctx.badRequest([
