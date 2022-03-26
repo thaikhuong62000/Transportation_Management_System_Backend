2b6e02bb9851fbcc317aa095b22904178e4912d4
thaikhuong62000 <thaikhuong62000@gmail.com>
2022-03-26 09:47:56 +0700

    Update server port

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  port: portNumber(env("NODE_ENV")),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "0441af3dad953748a97088752b630471"),
@@ -13,3 +13,16 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+const portNumber = (env) => {
+  switch (env) {
+    case "development":
+      return 1337;
+    case "staging":
+      return 1338;
+    case "production":
+      return 1339;
+    default:
+      return 1337;
+  }
+};
