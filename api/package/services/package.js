c582071c1787ce4ac5f52afe4f55a42011a3f6f1
AnhKhoa1408 <akhoa981@gmail.com>
2022-04-03 00:18:31 +0700

    remove unuse service

--- api/package/services/package.js
+++ api/package/services/package.js
@@ -6,22 +6,4 @@ var mongoose = require("mongoose");
  * to customize this service
  */
 
-module.exports = {
-  async getPackagesInStorage(id, limit, skip) {
-    let entities = await strapi
-      .query("import")
-      .model.find({ storage: mongoose.Types.ObjectId(id) })
-      .populate("package", [
-        "size",
-        "current_address",
-        "position",
-        "quantity",
-        "package_type",
-        "weight",
-      ])
-      .limit(limit)
-      .skip(skip);
-
-    return entities;
-  },
-};
+module.exports = {};
