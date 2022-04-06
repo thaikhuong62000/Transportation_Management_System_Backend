c8835134d21fbdf3be52e04a0f052ac78c9773a0
thaikhuong62000 <thaikhuong62000@gmail.com>
2022-04-06 20:05:30 +0700

    Update create shipment

--- api/shipment/controllers/shipment.js
+++ api/shipment/controllers/shipment.js
@@ -53,26 +53,9 @@ module.exports = {
       .query("shipment")
       .model.findOneAndUpdate({ _id, driver: null }, { driver }, { new: true })
       .populate("packages");
-    if (shipment.packages.length > 0) {
-      let orders = shipment.packages.map((item) => item.order);
-      let packages = shipment.packages.map((item) => item._id);
-      await strapi.services.order.update(
-        {
-          _id: { $in: orders },
-          state: 0,
-        },
-        { state: 1 },
-        { multi: true }
-      );
-      await strapi.services.packages.update(
-        {
-          _id: { $in: packages },
-          state: 0,
-        },
-        { state: 1 },
-        { multi: true }
-      );
-    }
+
+    strapi.services.shipment.updateOrderState(shipment);
+
     return shipment;
   },
 
@@ -112,170 +95,25 @@ module.exports = {
     };
   },
 
-  /**
-   * Note for transaction:
-   *    use _id instead of id
-   *    in create method, pass { session: session } instead of chain .session(session)
-   *    in create method, value must enclosed in [] or it will create duplicated entries
-   *    if findOneAndUpdate result is use for next step, pass option { new: true }
-   */
   async create(ctx) {
-    let {
-      orderId,
-      vehicleId,
-      shipmentInfo,
-      newOrderInfo,
-      updateQuantityList = [], // {id, quantity}
-      updatePackageList = [], // {id},
-      removePackageList = [], // {id}
-      newPackageList = [],
-      orderState = null, // number
-    } = ctx.request.body;
-    let shipment = "";
+    const { vehicleId, driver } = ctx.request.body;
 
-    const db = strapi.connections.default;
-    const session = await db.startSession();
-    session.startTransaction();
-    const { Package, Order, Shipment, Car } = db.models; // Models
+    const shipment = await strapi.services.shipment.create(ctx.request.body);
 
-    try {
-      if (newOrderInfo) {
-        // Update quantity of package for old order
-        if (updateQuantityList.length) {
-          for (let item of updateQuantityList) {
-            const _package = await Package.findOneAndUpdate(
-              { _id: item.id },
-              { quantity: item.quantity }
-            ).session(session);
-            if (!_package) throw "Update package failed";
-          }
-        }
-
-        // Remove relation of package have full quantity for shipment
-        if (updatePackageList.length) {
-          const order = await Order.findOneAndUpdate(
-            { _id: orderId },
-            { packages: updatePackageList }
-          ).session(session);
-          if (!order) throw "Update order failed";
-        }
-
-        // Create package for order
-        let newOrder = await Order.create([newOrderInfo], {
-          session: session,
-        });
-        if (newPackageList) {
-          for (let pack of newPackageList) {
-            await Package.create(
-              [
-                {
-                  ...pack,
-                  state: 1,
-                  order: newOrder._id,
-                },
-              ],
-              { session: session }
-            );
-          }
-        }
-
-        // Add package relation
-        let insertedOrder = await Order.findOne({
-          _id: newOrder._id,
-        }).session(session);
-        if (!insertedOrder) throw "Not found order";
-        if (removePackageList.length) {
-          for (let item of removePackageList) {
-            let pack = await Package.findOneAndUpdate(
-              {
-                _id: item,
-              },
-              {
-                state: 1,
-              }
-            ).session(session);
-
-            if (!pack) throw "Cannot update old order's package state";
-          }
-
-          insertedOrder = await Order.findOneAndUpdate(
-            {
-              _id: insertedOrder._id,
-            },
-            {
-              packages: [
-                ...insertedOrder.packages.map((item) => item._id),
-                ...removePackageList,
-              ],
-            },
-            { new: true }
-          ).session(session);
-        }
-
-        // Create shipment
-        shipment = await Shipment.create(
-          [
-            {
-              ...shipmentInfo,
-              packages: insertedOrder.packages.map((item) => item._id),
-            },
-          ],
-          { session: session }
-        );
-
-        // Update car current shipments
-        const car = await Car.findOneAndUpdate(
-          {
-            _id: vehicleId,
-          },
-          {
-            $push: {
-              shipments: shipment._id,
-            },
-          }
-        ).session(session);
-        if (!car) throw "Car not found";
-      } else {
-        shipment = await Shipment.create([shipmentInfo], { session: session });
-
-        //  Update current shipments for car
-        const car = await Car.findOneAndUpdate(
-          { _id: vehicleId },
-          {
-            $push: {
-              shipments: shipment._id,
-            },
-          }
-        ).session(session);
-        if (!car) throw "Update car failed";
-
-        // Update order state and package state
-        if (orderState && orderState !== null) {
-          await Order.findOneAndUpdate(
-            { _id: orderId },
-            { state: orderState }
-          ).session(session);
-          await Package.updateMany(
-            { order: orderId },
-            { state: orderState }
-          ).session(session);
-        }
-      }
-      await session.commitTransaction();
-      session.endSession();
-      return shipment;
-    } catch (error) {
-      console.log(error);
-      await session.abortTransaction();
-      session.endSession();
-      return ctx.badRequest(null, {
-        errors: [
-          {
-            id: "Shipment.create",
-            message: "Bad Request",
-          },
-        ],
-      });
+    if (driver) {
+      strapi.services.shipment.updateOrderState(shipment);
     }
+
+    //  Update current shipments for car
+    await strapi.services.car.findOneAndUpdate(
+      { _id: vehicleId },
+      {
+        $push: {
+          shipments: shipment._id,
+        },
+      }
+    );
+
+    return shipment;
   },
 };
