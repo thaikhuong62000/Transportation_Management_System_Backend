933ee8bc3f7a3f72125ace637ac7fa526296dab7
DuongMinhNhiLong <65639524+DuongMinhNhiLong@users.noreply.github.com>
2022-04-23 06:52:45 +0700

    tét voucher

--- tests/voucher/updateVoucherImage.js
+++ tests/voucher/updateVoucherImage.js
@@ -1,12 +1,46 @@
 const request = require("supertest");
 const mockUserData = {
-  username: "FujiwaraChika",
+  username: "FujiwaraChika", //user customer
   password: "12345678",
-  adminUsername:"admin",
-  adminPassword:"12345678"
+  adminUsername: "admin",
+  adminPassword: "12345678",
 };
 
-it("kiểm tra quyền user không dc update voucher", async () => {
+const mockAvatar = [{ avatar: "public/uploads/voucher_pic0.jpg" }];
+const image = "public/uploads/voucher_pic0.jpg";
+// const mockVoucherData = [
+//   { password: "12345678", newPassword: "12345678", expected: "update" },
+//   { password: "123456789", newPassword: "12345678", expected: "not update" },
+//   { password: "12345678", newPassword: "123456789", expected: "update" },
+//   { password: "123456789", newPassword: "", expected: "not update" },
+// ];
+
+it.each(mockAvatar)("kiểm tra quyền admin update voucher", async () => {
+  const jwt = await request(strapi.server)
+    .post("/auth/local")
+    .set("accept", "application/json")
+    .set("Content-Type", "application/json")
+    .send({
+      identifier: mockUserData.adminUsername,
+      password: mockUserData.adminPassword,
+    })
+    .expect("Content-Type", /json/)
+    .expect(200)
+    .then((data) => {
+      expect(data.body.jwt).toBeDefined();
+      return data.body.jwt;
+    });
+  await request(strapi.server)
+    .post("/vouchers/image")
+    .set("accept", "*")
+    .set("Authorization", "Bearer " + jwt)
+    .attach("image",image)
+    .expect("Content-Type", /json/)
+    .expect(200);
+
+});
+
+it.each(mockAvatar)("kiểm tra quyền user không dc update voucher", async () => {
   const jwt = await request(strapi.server)
     .post("/auth/local")
     .set("accept", "application/json")
@@ -26,36 +60,7 @@ it("kiểm tra quyền user không dc update voucher", async () => {
     .set("accept", "application/json")
     .set("Content-Type", "application/json")
     .set("Authorization", "Bearer " + jwt)
-    .send({
-        files: { image: "_image" },
-    })
+    .attach("image",image)
     .expect("Content-Type", /json/)
     .expect(403);
 });
-
-// it("kiểm tra quyền user không dc update voucher", async () => {
-//     const jwt = await request(strapi.server)
-//       .post("/auth/local")
-//       .set("accept", "application/json")
-//       .set("Content-Type", "application/json")
-//       .send({
-//         identifier: mockUserData.adminUsername,
-//         password: mockUserData.adminPassword,
-//       })
-//       .expect("Content-Type", /json/)
-//       .expect(200)
-//       .then((data) => {
-//         expect(data.body.jwt).toBeDefined();
-//         return data.body.jwt;
-//       });
-//     await request(strapi.server)
-//       .post("/vouchers/image")
-//       .set("accept", "application/json")
-//       .set("Content-Type", "application/json")
-//       .set("Authorization", "Bearer " + jwt)
-//       .send({},{
-//           image: 'Pictures/278455850_566423334621521_1622723246964081647_n.jpg' ,
-//       })
-//       .expect("Content-Type", /json/)
-//       .expect(200);
-//   });
