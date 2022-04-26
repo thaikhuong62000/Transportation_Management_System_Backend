f019084d14d857ca3885c58abe127a53e86983fe
thaikhuong62000 <thaikhuong62000@gmail.com>
2022-04-26 18:20:41 +0700

    Add get nearest stoarge

--- api/shipment/services/shipment.js
+++ api/shipment/services/shipment.js
@@ -186,6 +186,20 @@ module.exports = {
     ]);
     return shipments;
   },
+  coordToDistance(lat1, lon1, lat2, lon2) {
+    const R = 6371; // Radius of the earth in km
+    const dLat = deg2rad(parseFloat(lat2) - parseFloat(lat1)); // deg2rad below
+    const dLon = deg2rad(parseFloat(lon2) - parseFloat(lon1));
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(deg2rad(parseFloat(lat1))) *
+        Math.cos(deg2rad(parseFloat(lat2))) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c; // Distance in km
+    return d;
+  },
 };
 
 function sortShipmentByDistance(lat, lng, item1, item2, sort_fa = false) {
@@ -199,12 +213,12 @@ function sortShipmentByDistance(lat, lng, item1, item2, sort_fa = false) {
 
 function coordToDistance(lat1, lon1, lat2, lon2) {
   const R = 6371; // Radius of the earth in km
-  const dLat = deg2rad(lat2 - lat1); // deg2rad below
-  const dLon = deg2rad(lon2 - lon1);
+  const dLat = deg2rad(parseFloat(lat2) - parseFloat(lat1)); // deg2rad below
+  const dLon = deg2rad(parseFloat(lon2) - parseFloat(lon1));
   const a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(deg2rad(lat1)) *
-      Math.cos(deg2rad(lat2)) *
+    Math.cos(deg2rad(parseFloat(lat1))) *
+      Math.cos(deg2rad(parseFloat(lat2))) *
       Math.sin(dLon / 2) *
       Math.sin(dLon / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
