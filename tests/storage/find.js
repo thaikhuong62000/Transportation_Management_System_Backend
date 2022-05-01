0ec81366ca7c172d4b5c855773c5b7d83ef731d7
DuongMinhNhiLong <65639524+DuongMinhNhiLong@users.noreply.github.com>
2022-05-02 06:41:07 +0700

    test storage

---
+++ tests/storage/find.js
@@ -0,0 +1,30 @@
+const request = require("supertest");
+const { jwtToken } = require("../__mocks__/AuthMocks");
+
+const testCaseData = [
+  {
+    message: "stocker find storage",
+    expect: 200,
+    type: "stocker",
+  },
+  {
+    message: "stocker find storage",
+    expect: 403,
+    type: "driver",
+  },
+  {
+    message: "stocker find storage",
+    expect: 403,
+    type: "customer",
+  },
+];
+
+it.each(testCaseData)("$message", async ({ expect, type }) => {
+  await request(strapi.server)
+    .get("/storages")
+    .set("accept", "application/json")
+    .set("Content-Type", "application/json")
+    .set("Authorization", "Bearer " + jwtToken(type))
+    .expect("Content-Type", expect === 404 ? /text/ : /json/)
+    .expect(expect);
+});
