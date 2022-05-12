383a7c14d6748a7e746bb06f34df461e2d283197
thaikhuong62000 <thaikhuong62000@gmail.com>
2022-05-12 13:53:03 +0700

    Fix address error

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -220,18 +220,14 @@ module.exports = {
       // Check coords of addresses
       // Calculate fee
       // Apply voucher
-      try {
-        fee = await strapi.services.fee.calcFee(
-          from_address,
-          to_address,
-          packages,
-          ctx.state.user,
-          voucher
-        );
-        remain_fee = fee;
-      } catch (error) {
-        throw error;
-      }
+      fee = await strapi.services.fee.calcFee(
+        from_address,
+        to_address,
+        packages,
+        ctx.state.user,
+        voucher
+      );
+      remain_fee = fee;
 
       session = await db.startSession();
       session.startTransaction();
@@ -291,12 +287,14 @@ module.exports = {
       ]);
     } catch (error) {
       console.log(error);
-      await session.abortTransaction();
-      session.endSession();
+      if (session) {
+        await session.abortTransaction();
+        session.endSession();
+      }
       return ctx.badRequest([
         {
           id: "order.create",
-          message: JSON.stringify(error),
+          message: error,
         },
       ]);
     }
