8d12535f6ab7e76bf30d24c8f2f0597f932d9c2c
thaikhuong62000 <thaikhuong62000@gmail.com>
2022-05-12 14:37:54 +0700

    Update fee

--- extensions/users-permissions/controllers/Fee.js
+++ extensions/users-permissions/controllers/Fee.js
@@ -2,48 +2,47 @@
 
 module.exports = {
   async getFeeFromOrder(ctx) {
-    const {
-      from_address,
-      to_address,
-      voucher = "",
-      packages = [],
-    } = ctx.request.body;
+    try {
+      const {
+        from_address,
+        to_address,
+        voucher = "",
+        packages = [],
+      } = ctx.request.body;
 
-    const { id } = ctx.state.user;
+      const { id } = ctx.state.user;
 
-    if (
-      !from_address ||
-      !to_address ||
-      !Object.keys(from_address).length ||
-      !Object.keys(to_address).length ||
-      !Object.values(from_address).length ||
-      !Object.values(to_address).length
-    ) {
+      if (
+        !from_address ||
+        !to_address ||
+        !Object.keys(from_address).length ||
+        !Object.keys(to_address).length ||
+        !Object.values(from_address).length ||
+        !Object.values(to_address).length
+      ) {
+        throw "Invalid address";
+      }
+
+      if (!packages || (Array.isArray(packages) && !packages.length)) {
+        throw "Invalid packages";
+      }
+
+      let fee = await strapi.services.fee.calcFee(
+        from_address,
+        to_address,
+        packages,
+        id,
+        voucher
+      );
+
+      return fee;
+    } catch (error) {
       return ctx.badRequest([
         {
           id: "Fee.getFeeFromOrder",
-          message: "Invalid address",
+          message: error,
         },
       ]);
     }
-
-    if (!packages || (Array.isArray(packages) && !packages.length)) {
-      return ctx.badRequest([
-        {
-          id: "Fee.getFeeFromOrder",
-          message: "Invalid packages",
-        },
-      ]);
-    }
-
-    let fee = await strapi.services.fee.calcFee(
-      from_address,
-      to_address,
-      packages,
-      id,
-      voucher
-    );
-
-    return fee;
   },
 };
