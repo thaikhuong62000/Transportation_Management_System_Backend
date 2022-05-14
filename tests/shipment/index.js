f4b7fa95ab1a78acc60d6b81223447d226fb549c
DuongMinhNhiLong <65639524+DuongMinhNhiLong@users.noreply.github.com>
2022-05-14 17:22:50 +0700

    test shipment "demo"

---
+++ tests/shipment/index.js
@@ -0,0 +1,6 @@
+// require("./acceptShipment");
+require("./create");
+require("./finishShipment");
+require("./getCurrent-shipments");
+require("./getFinishedShipment");
+require("./getVehicleDetailByShipment");
