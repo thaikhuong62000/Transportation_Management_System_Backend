8729669e745e351722a69da130a52e955c5966bb
thaikhuong62000 <thaikhuong62000@gmail.com>
2022-05-21 19:29:16 +0700

    Fix bug

--- tests/testsuite1/export.js
+++ tests/testsuite1/export.js
@@ -1,33 +1,33 @@
 const request = require("supertest");
 const { createdOrder } = require("../__mocks__/OrderMocks");
 const { jwtToken } = require("../__mocks__/AuthMocks");
+const { variable } = require("../__mocks__/Global");
 
-module.exports = (shipment) => {
-  const _request = request(strapi.server)
+it("stocker2 export packages fail", async () => {
+  await request(strapi.server)
     .put("/exports/update-quantity")
     .set("accept", "application/json")
-    .set("Content-Type", "application/json");
+    .set("Content-Type", "application/json")
+    .set("Authorization", "Bearer " + jwtToken("stocker2"))
+    .send({
+      quantity: 11,
+      shipment: variable("shipment").id,
+    })
+    .expect("Content-Type", /json/)
+    .expect(400);
+});
 
-  it("stocker2 export packages fail", async () => {
-    await _request
-      .set("Authorization", "Bearer " + jwtToken("stocker2"))
-      .send({
-        quantity: 11,
-        shipment: shipment.id,
-      })
-      .expect("Content-Type", /json/)
-      .expect(400);
-  });
-
-  it("stocker2 export packages fail", async () => {
-    await _request
-      .set("Authorization", "Bearer " + jwtToken("stocker2"))
-      .send({
-        packageId: createdOrder("order").packages[0].id,
-        quantity: -11,
-        shipment: shipment.id,
-      })
-      .expect("Content-Type", /json/)
-      .expect(400);
-  });
-};
+it("stocker2 export packages fail", async () => {
+  await request(strapi.server)
+    .put("/exports/update-quantity")
+    .set("accept", "application/json")
+    .set("Content-Type", "application/json")
+    .set("Authorization", "Bearer " + jwtToken("stocker2"))
+    .send({
+      packageId: createdOrder("order").packages[0].id,
+      quantity: -11,
+      shipment: variable("shipment").id,
+    })
+    .expect("Content-Type", /json/)
+    .expect(400);
+});
