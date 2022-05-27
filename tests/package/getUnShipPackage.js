64ace61e7cc811cbc47ffed6e452f48a65208ab1
DuongMinhNhiLong <65639524+DuongMinhNhiLong@users.noreply.github.com>
2022-05-27 08:38:07 +0700

    test get packages

---
+++ tests/package/getUnShipPackage.js
@@ -0,0 +1,20 @@
+const request = require("supertest");
+const { jwtToken } = require("../__mocks__/AuthMocks");
+const testCaseData = [
+  {
+    message: "get getUnShipPackage by admin response 200 ",
+    expect: 200,
+    type: "admin",
+    storeId: "6252b34e5eedf42d04bd5147",
+  },
+];
+
+it.each(testCaseData)("$message", async ({ expect, type, storeId }) => {
+  await request(strapi.server)
+    .get("/packages/unship/" + storeId)
+    .set("accept", "application/json")
+    .set("Content-Type", "application/json")
+    .set("Authorization", "Bearer " + jwtToken(type))
+    .expect("Content-Type", /json/)
+    .expect(expect);
+});
